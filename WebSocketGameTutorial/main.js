1d3aeadc6ebab4e5dddb79acc75dc9df2b8c3576
Dmutre <104783173+Dmutre@users.noreply.github.com>
2024-03-31 16:34:39 +0200

    update

---
+++ WebSocketGameTutorial/main.js
@@ -0,0 +1,10 @@
+'use strict';
+const express = require('express');
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log('Server started on PORT: ' + PORT);
+});
